7ab056e76f53cde46ec635110922f2d9f80aea3a
adheeb <adheebasifofficial@gmail.com>
2024-08-14 12:24:41 +0530

    comming soon page

---
+++ src/app/(web)/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image from 'next/image'
+
+function page() {
+  return (
+    <div className='flex items-center justify-center flex-col w-full h-screen'>
+      <h1>
+        <Image 
+            src={'/logo.png'}
+            width={200}
+            height={200}
+            alt='Logo'
+        />
+      </h1>
+      <p className='mt-6 font-bold text-2xl'>Coming Soon !</p>
+    </div>
+  )
+}
+
+export default page
