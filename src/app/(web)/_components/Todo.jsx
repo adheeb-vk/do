7c6c38e28351a5017cc9e03487230e2d810b80ca
adheeb <adheebasifofficial@gmail.com>
2024-09-02 00:48:46 +0530

    updated features

--- src/app/(web)/_components/Todo.jsx
+++ src/app/(web)/_components/Todo.jsx
@@ -6,67 +6,69 @@ import Image from 'next/image'
 function Todo() {
 
 const [value, setValue] = useState("");
+const [toDos, setToDos] = useState([]);
+const [completedTodos, setCompletedTodos] = useState([])
+const [description, setDescription] = useState("")
 
-const [toDos, setToDos] = useState(Array);
-
-const [storedData, setStoredData] = useState(window.localStorage.getItem("todo_list"))
-// const [localStorageToDos, setLocalStorageToDos] = useState(Array)
-
-// useEffect(()=>{
-//     const stored_data = window.localStorage.getItem("todo_list")
-//     if(stored_data){
-//         setLocalStorageToDos(stored_data)
-//         console.log(stored_data)
-//     }else{
-//         window.localStorage.setItem("todo_list", [])
-//         console.log(stored_data);
-//     }
-// },[])
-
-// useEffect(()=>{
-    
-//     console.log(storedData)
-// },[])
+const [isNewTodo, setIsNewTodo] = useState(false)
 
 useEffect(()=>{
-    if(storedData !== null){
-        if(storedData.length > 2){
-            let stored_data = JSON.parse(storedData)
-            if(toDos.length !== 0){
-                stored_data.push(toDos)
-                localStorage.removeItem("todo_list")
-                localStorage.setItem("todo_list", JSON.stringify(stored_data))
-            }
-        }else{
-            if(toDos.length !== 0){
-                console.log(toDos.length)
-                localStorage.removeItem("todo_list")
-                localStorage.setItem("todo_list", JSON.stringify(toDos))
-            }
-        }
-    }else{
-        window.localStorage.setItem("todo_list", JSON.stringify([]))
-        // console.log(storedData);
+    const savedTodos = JSON.parse(localStorage.getItem("todo_list"))
+    const completed_todos = JSON.parse(localStorage.getItem("completed_list"))
+    if(savedTodos){
+        setToDos(savedTodos)
+    }
+    if(completed_todos){
+        setCompletedTodos(completedTodos)
+    }
+},[])
+
+useEffect(()=>{
+    if(toDos.length > 0){
+        localStorage.setItem("todo_list", JSON.stringify(toDos))
     }
 },[toDos])
+
+useEffect(()=>{
+    if(completedTodos.length > 0){
+        localStorage.setItem("completed_list", JSON.stringify(completedTodos))
+    }
+},[completedTodos])
 
 const removeToDo = (id) => {
     let editedtoDos = toDos.filter(toDo => toDo.id !== id)
     setToDos(editedtoDos)
 }
 
-const addToDO = (e) =>{
+const addToDO = () =>{
     let newToDo = {
         id: toDos?.length + 1,
-        title: value
+        title: value,
+        description: description
     }
-    setToDos([newToDo,...toDos])
+    setToDos([...toDos,newToDo])
 }
 
-// useEffect(()=>{
-//     localStorageToDos.push(toDos)
-//    window.localStorage.setItem("todo_list",JSON.stringify(localStorageToDos))
-// },[toDos])
+const handleInput = (e) => {
+    e.preventDefault()
+    setValue(e.target.value)
+}
+
+const addToCompleted = (id) => {
+    let new_completed_todo = toDos.filter(toDo => toDo.id === id)
+    if(completedTodos.length > 0){
+        setCompletedTodos([...completedTodos,new_completed_todo])
+    }else{
+        setCompletedTodos([new_completed_todo])
+    }
+    let editedtoDos = toDos.filter(toDo => toDo.id !== id)
+    if(toDos.length === 1){
+        setToDos(editedtoDos)
+        localStorage.removeItem("todo_list")
+    }else{
+        setToDos(editedtoDos)
+    }
+}
 
 const date = new Date();
 const year = date.getFullYear();
@@ -76,55 +78,101 @@ const month = date.getMonth();
   return (
     <div className='w-full'>
       <div className='wrapper'>
+        {/* <button className='py-3 px-4 bg-red-400' onClick={(e)=>{
+            e.preventDefault()
+            if(completedTodos.length > 0){
+            console.log(completedTodos)
+            }
+        }}>test</button> */}
         <div className='w-[40%] max-[1280px]:w-[50%] max-[1080px]:w-[60%] max-[980px]:w-[70%] max-[580px]:w-[80%] max-[480px]:w-[90%] mx-[auto]'>
             <h1 className='text-center font-bold text-2xl mb-6'>Things to DO !</h1>
             <div className='mb-6'>
                 <span>{`DATE: ${day}/${month}/${year}`}</span>
             </div>
-            <div className='flex bg-transparent border-2 border-solid rounded-md border-white py-2 px-2 mb-6'>
-                <input value={value} onChange={(e)=> 
-                    {e.preventDefault()
-                    setValue(e.target.value)}} className='w-full bg-transparent outline-none font-semibold px-3' maxLength={40} placeholder='What to DO?' type='text'/>
+            <button className={`${isNewTodo ? "bg-red-500" : "bg-green-700"} text-white py-2 px-4 text-black font-bold text-center rounded-md mb-2`} onClick={()=>{
+                if(isNewTodo){
+                    setIsNewTodo(false)
+                }else{
+                    setIsNewTodo(true)
+                }
+            }}>{isNewTodo ? "Back" : "Add New"}</button>
+            {isNewTodo ? 
+            <div className='flex flex-col bg-transparent border-2 border-solid rounded-md border-white py-2 px-2 mb-6'>
+                <input value={value}
+                    onChange={(e)=> {
+                        handleInput(e)
+                    }}
+                    className='w-full bg-transparent text-white mb-2 border-2 rounded-md py-2 border-solid border-white outline-none font-semibold px-3' maxLength={40} placeholder='Enter Title' type='text'/>
+                <textarea value={description}
+                    onChange={(e)=> {
+                        e.preventDefault()
+                        setDescription(e.target.value)
+                        console.log(description)
+                    }}
+                    className='w-full bg-transparent text-white border-2 rounded-md py-2 border-solid border-white mb-2 outline-none px-3' placeholder='Enter Description'/>
                 <button type='submit' className='py-2 px-3 bg-white text-black font-bold rounded-md' onClick={(e)=>{
                     e.preventDefault()
                     if(value !== ""){
                         addToDO(e)
                         setValue("")
+                        setDescription("")
+                        setIsNewTodo(false)
                     }
                 }}>Add</button>
-            </div>
+            </div> :
             <div className='p-[30px]'>
                 {toDos?.length === 0 ? 
-                <h1 className='font-bold text-xl text-center max-[380px]:px-[30px]'>
+                <h1 className='font-bold text-xl text-center max-[380px]:text-[18px] max-[345px]:text-[16px]'>
                     Create your new ToDo list
                 </h1> :
+                <>
                 <ul>
                     {toDos?.map((toDo)=>{
                         return(
-                        <li className='w-full flex items-center justify-between mb-6' key={toDo.id}>
-                            <div className='flex items-center gap-4'>
-                                <div className='w-5 h-5 rounded-xl bg-red-400 border-2 border-solid border-red-600'></div>
+                        <li className='w-full flex items-center justify-between mb-6 border p-2 border-solid border-white' key={toDo.id}>
+                            <div className='flex items-center gap-4 max-[800px]:w-[40%]'>
+                                <div>{toDo.id}</div>
                                 <p>{toDo.title}</p>
                             </div>
-                            <button onClick={(e)=>{
-                                e.preventDefault()
-                                window.localStorage.clear()
-                                removeToDo(toDo.id)
-                                }
-                                }>
-                                <Image
-                                src={require('../../assets/icons/delete.svg')}
-                                width={25}
-                                height={25}
-                                alt='delete'
-                                />
-                            </button>
+                            <div className='flex items-center'>
+                                <button className='mr-4' 
+                                onClick={(e)=>{
+                                    e.preventDefault()
+                                    addToCompleted(toDo.id)
+                                }}
+                                >
+                                    <Image
+                                    src={require('../../assets/icons/check.svg')}
+                                    width={19}
+                                    height={19}
+                                    alt='check'
+                                    />
+                                </button>
+                                <button onClick={(e)=>{
+                                    e.preventDefault()
+                                    window.localStorage.clear()
+                                    removeToDo(toDo.id)
+                                    }
+                                    }>
+                                    <Image
+                                    src={require('../../assets/icons/delete.svg')}
+                                    width={25}
+                                    height={25}
+                                    alt='delete'
+                                    />
+                                </button>
+                            </div>
                         </li>
                         )
                     })}
                 </ul>
+                <button className='py-2 px-3 text-center text-black w-full bg-white rounded-md font-bold' onClick={()=>{
+                    localStorage.removeItem("todo_list")
+                    setToDos([])
+                }}>Clear All</button>
+                </>
                 }   
-            </div>
+            </div>}
         </div>
       </div>
     </div>
--- src/app/(web)/page.jsx
+++ src/app/(web)/page.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Header from './_components/Header'
 import Todo from './_components/Todo'
-import TodoTest from  './_components/TodoTest'
 
 function page() {
   return (
     <>
       <Header/>
-      <TodoTest/>
-      {/* <Todo/> */}
+      <Todo/>
     </>
   )
 }
